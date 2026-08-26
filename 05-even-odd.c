9be37ab91354f977be3119a3795ac41dc0556fdd
Riya-oni <rn198306@gmail.com>
2026-08-26 21:32:24 +0530

    Add even-odd check program in 05-even-odd.c

---
+++ 05-even-odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = 7;
+
+    if (num % 2 == 0) {
+        printf("Even number");
+    } else {
+        printf("Odd number");
+    }
+
+    return 0;
+}
